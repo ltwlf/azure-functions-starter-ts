fa340721a67cfcd11420a527058c0ea7fefc127c
Christian Glessner <chris@leitwolf.io>
2020-05-08 18:52:52 +0000

    Setup jest and sample test

---
+++ hello/index.test.ts
@@ -0,0 +1,15 @@
+import { httpTrigger } from './index'
+
+const context =  { log: jest.fn() } as any
+
+test('Hello schould return greeting', async () => {
+
+    const request = {
+        query: { name: 'Chris' }
+    };
+
+    await httpTrigger(context, request)
+
+    expect(context.log.mock.calls.length).toBe(1)
+    expect(context.res.body).toEqual('Hello Chris')
+});
